138848d2b83d4ad2ccdd5cb75a4d2090b583f7d1
Preston Elliott <preston.elliott24@gmail.com>
2019-04-09 16:19:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,13 @@
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
+end
+
 # def reverse_each_word(string)
 #   array = string.split(" ")
 #   array.each do |word|
 #     word.reverse
 #   end
 # end
-
-
-def reverse_each_word(string)
-  string.split.collect do
-    |word| word.reverse.join(" ")
-  end
-end
-
-
-def reverse_each_word(string)
-  string.split.collect {|word| word.reverse}.join(" ")
-end
 
 # def reverse_each_word(string)
 #     array = []
