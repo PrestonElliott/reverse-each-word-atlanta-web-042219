3c38bbeda6c192ec09c0680e9bc4fad37f0637ed
Preston Elliott <preston.elliott24@gmail.com>
2019-04-09 16:16:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,19 +5,23 @@
 #   end
 # end
 
-# example that reverses full sentence instead of each word
-# output: "?uoy era woh dna ,ereht olleH"
-# expect: "olleH ,ereht dna woh era ?uoy"
+
 def reverse_each_word(string)
-    array = []
-    reverse_array = []
-    i = string.length
-array.push(string.split(""))
-array.each do |item|
-    until i < 0
-        reverse_array.push(item[i])
-        i -= 1
-    end
+  string.split.collect do
+    |word| word.reverse.join(" ")
+  end
 end
-reverse_array.join
-end
+
+# def reverse_each_word(string)
+#     array = []
+#     reverse_array = []
+#     i = string.length
+# array.push(string.split(""))
+# array.each do |item|
+#     until i < 0
+#         reverse_array.push(item[i])
+#         i -= 1
+#     end
+# end
+# reverse_array.join
+# end
